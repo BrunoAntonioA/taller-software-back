e7fec069d6d61a29755e3c4836bd287eef6fadc0
Rodrigo Maureira Contreras <rodmaureirac@gmail.com>
2020-01-12 00:53:58 -0300

    mejoras menores

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => res.send('Bienvenido al BACK-END para el Taller de Software'))
+router.get('/', (req, res) => res.send('Bienvenido al BACK-END para el Taller de Software\n'))
 
 module.exports = router
