bf50c4295f0a70beefe3446037ef2a7b83c8d6f9
DGJumpa <jpraabm@hotmail.com>
2020-01-11 21:18:44 -0300

    agregué proyecto

---
+++ server/models/Proyecto.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose')
+
+/* 
+ * Esquema base para almacenar
+ * la información de los usuarios
+ * dentro de MongoDB. Las contraseñas
+ * se almacenen mediante el hash
+ * correspondiente.
+ * 
+ * Más info en  /server/routes/register
+ *              /server/routes/login
+ * 
+ * Autores: Felipe Céspedes Cordero
+ * 
+ */ 
+
+const ProyectSchema = new mongoose.Schema({
+    
+    /*Nombre del proyecto */
+    nombre_proyecto:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: false,
+    },
+
+
+    /* Correo electrónico del jefe de proyecto */
+    email_jefe: {
+        type: String, 
+        required: true, 
+        trim: true, 
+        unique: false, 
+    },
+
+    /*Nombre del proyecto */
+    nombre_jefe_proyecto:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: false,
+    },
+    
+    /*Nota de la primera evaulación del proyecto */
+    nota_uno:{
+        type: Number,
+        required: false,
+        trim: true,
+        unique: false,
+    },
+
+    /*Nota de la segunda evaulación del proyecto */
+    nota_dos:{
+        type: Number,
+        required: false,
+        trim: true,
+        unique: false,
+    },
+    
+})
+
+/* Módulo a exportar */
+module.exports = mongoose.model('User', UserSchema)
---
+++ server/routes/proyecto.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const Proyecto = require('../models/Proyecto')
+
+router.post('/proyecto', (req, res) => {
+    const nombre_proyecto = req.body.nombre_proyecto
+    const email_jefe = req.body.email_jefe
+    const nombre_jefe_proyecto = req.body.nombre_jefe_proyecto
+    const nota_uno = req.body.nota_uno
+    const nota_dos = req.body.nota_dos
+    const newProyeto = new Proyecto({ 
+        nombre_proyecto: nombre_proyecto, 
+        email_jefe: email_jefe, 
+        nombre_jefe_proyecto: nombre_jefe_proyecto,
+        nota_uno: nota_uno, 
+        nota_dos: nota_dos
+    })
+})
+
+router.get('/proyecto', (req, res) => {
+      Proyecto.find((error, proyecto) => {
+        if(error) return res.status(401).send({message: 'Already registered'})
+        return res.status(201).send(proyecto)
+    })
+})
+
+module.exports = router
