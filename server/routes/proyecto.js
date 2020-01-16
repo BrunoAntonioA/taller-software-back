0b51eaf36cde2bbc4fcf4b8ed7632d9e8831f880
Rodrigo Maureira Contreras <rodmaureirac@gmail.com>
2020-01-16 05:39:44 -0300

    agfregue update

--- server/routes/proyecto.js
+++ server/routes/proyecto.js
@@ -63,4 +63,14 @@ router.post('/proyecto/eliminar/:id', (req, res) => {
     })
 })
 
+
+// Elimina un proyecto según su id
+router.post('/proyecto/actualizar/', (req, res) => {
+    const body = req.body
+    Proyecto.findOneAndUpdate({ _id: id }, { $set: req.body }, (error) => {
+        if (error) return res.status(404).send({ message: 'Error' })
+        return res.status(201).send({ message: "Done!" })
+    })
+})
+
 module.exports = router
